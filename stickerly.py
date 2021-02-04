280e81da05279ab7c7c2320130a1f2c381d39498
Mustafa <42353725+ciwga@users.noreply.github.com>
2021-02-05 00:36:17 +0300

    050221

--- stickerly.py
+++ stickerly.py
@@ -1,33 +1,60 @@
+# _*_ coding:utf-8 _*_
 from bs4 import BeautifulSoup
 import requests
 import os
-
-try:
-    os.mkdir("Sticker.ly")
-except FileExistsError:
-    pass
-
-id_stickers = input("Enter the sticker code: ")
-site = "http://sticker.ly/s/" + id_stickers
-try:
-    img_links = []
-    r = requests.get(site)
-    soup = BeautifulSoup(r.content, "lxml")
-    content_images = soup.find("ul", attrs={"id": "content_images"})\
-        .find_all("li")
-    for src in content_images:
-        picture = src.find_all("picture")
-        for img in picture:
-            c_img = img.find("img").get("src")
-            img_links.append(c_img)
-except AttributeError:
-    print("\nCheck your sticker code\nThis is invalid code!")
+import time
 
 
-os.chdir("Sticker.ly")
-number = 0
-for link in img_links:
-    r_link = requests.get(link, stream=True).raw.read()
-    with open(str(number)+".png", "wb") as f:
-        f.write(r_link)
-        number = number + 1
+class Stickerly():
+
+    def __init__(self):
+        self.StickersId = input("Enter the sticker id: ")
+        self.website = "http://sticker.ly/s/" + self.StickersId
+        self.imgLinks = []
+
+    def SaveStickers(self, title, author):
+        try:
+            path = "stickerly/" + title
+            os.makedirs(path)
+            os.chdir(path)
+        except NotADirectoryError:
+            InvalidPath = "stickerly/Invalid Name"
+            os.makedirs(InvalidPath)
+            os.chdir(InvalidPath)
+        else:
+            pass
+
+        with open("log.txt", "w+", encoding="utf-8") as log:
+            log.write(f"Title: {title}\nAuthor: {author}\nCreated: {time.ctime()}\
+            \nWebsite: {self.website}")
+
+        number = 0
+        for link in self.imgLinks:
+            imgLink = requests.get(link, stream=True).raw.read()
+            with open(str(number)+".webp", "wb") as image:
+                image.write(imgLink)
+                number = number + 1
+        print("Done!")
+
+    def StickerData(self):
+        r = requests.get(self.website)
+        soup = BeautifulSoup(r.content, "lxml")
+        try:
+            title = soup.find("div", attrs={"class": "sticker_name"})\
+                .find("strong").text
+            author = soup.find("span", attrs={"class": "sticker_author"})\
+                .text.strip()
+            content_images = soup.find("ul", attrs={"id": "content_images"})\
+                .find_all("li")
+        except AttributeError:
+            print("\nCheck your sticker id.\nThat is an invalid id!")
+        for media in content_images:
+            picture = media.find_all("picture")
+            for images in picture:
+                src = images.find("img").get("src")
+                self.imgLinks.append(src)
+        self.SaveStickers(title, author)
+
+
+sticker = Stickerly()
+sticker.StickerData()
